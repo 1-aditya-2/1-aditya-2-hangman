94c3bda6ac25144451751381fab86a674aa77a6f
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2022-02-26 09:08:49 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+var words = ["lion","monkey","tiger","cake","game","strong","index","code","angular","java"];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+for (var i = 0; i < word.length; i++)
+{
+    answerArray[i] = "_";
+}
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];  
+
+function myfunc()
+{
+     var alpha=document.
+}
