17509a24c8381d0cbdfb6dfa1d76675a03ac5f5f
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2022-02-26 12:02:13 +0530

    final

--- script.js
+++ script.js
@@ -1,18 +1,262 @@
+function reset(){
+    document.location.reload(true);
+}
+var count=10;
 
-var words = ["lion","monkey","tiger","cake","game","strong","index","code","angular","java"];
 
-var word = words[Math.floor(Math.random() * words.length)];
-
-for (var i = 0; i < word.length; i++)
+function myfunc1()
 {
-    answerArray[i] = "_";
+     document.getElementById("a").style.background='#000000';
+     document.getElementById("a").disabled=true;
+     document.getElementById("a2").style.visibility='visible';
+}
+function myfunc2()
+{
+     document.getElementById("b").style.background='#000000';
+     document.getElementById("b").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc3()
+{
+     document.getElementById("c").style.background='#000000';
+     document.getElementById("c").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc4()
+{
+     document.getElementById("d").style.background='#000000';
+     document.getElementById("d").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc5()
+{
+     document.getElementById("e").style.background='#000000';
+     document.getElementById("e").disabled=true;
+     document.getElementById("a7").style.visibility='visible';
+}
+function myfunc6()
+{
+     document.getElementById("f").style.background='#000000';
+     document.getElementById("f").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc7()
+{
+     document.getElementById("g").style.background='#000000';
+     document.getElementById("g").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc8()
+{
+     document.getElementById("h").style.background='#000000';
+     document.getElementById("h").disabled=true;
+     document.getElementById("a1").style.visibility='visible';
+}
+function myfunc9()
+{
+     document.getElementById("i").style.background='#000000';
+     document.getElementById("i").disabled=true;
+     document.getElementById("a5").style.visibility='visible';
+}
+function myfunc10()
+{
+     document.getElementById("j").style.background='#000000';
+     document.getElementById("j").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc11()
+{
+     document.getElementById("k").style.background='#000000';
+     document.getElementById("k").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc12()
+{
+     document.getElementById("l").style.background='#000000';
+     document.getElementById("l").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc13()
+{
+     document.getElementById("m").style.background='#000000';
+     document.getElementById("m").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc14()
+{
+     document.getElementById("n").style.background='#000000';
+     document.getElementById("n").disabled=true;
+     document.getElementById("a6").style.visibility='visible';
+}
+function myfunc15()
+{
+     document.getElementById("o").style.background='#000000';
+     document.getElementById("o").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc16()
+{
+     document.getElementById("p").style.background='#000000';
+     document.getElementById("p").disabled=true;
+     document.getElementById("a3").style.visibility='visible';
+     document.getElementById("a4").style.visibility='visible';
+}
+function myfunc17()
+{
+     document.getElementById("q").style.background='#000000';
+     document.getElementById("q").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc18()
+{
+     document.getElementById("r").style.background='#000000';
+     document.getElementById("r").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc19()
+{
+     document.getElementById("s").style.background='#000000';
+     document.getElementById("s").disabled=true;
+     document.getElementById("a8").style.visibility='visible';
+     document.getElementById("a9").style.visibility='visible';
+}
+function myfunc20()
+{
+     document.getElementById("t").style.background='#000000';
+     document.getElementById("t").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc21()
+{
+     document.getElementById("u").style.background='#000000';
+     document.getElementById("u").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc22()
+{
+     document.getElementById("v").style.background='#000000';
+     document.getElementById("v").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc23()
+{
+     document.getElementById("w").style.background='#000000';
+     document.getElementById("w").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc24()
+{
+     document.getElementById("x").style.background='#000000';
+     document.getElementById("x").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc25()
+{
+     document.getElementById("y").style.background='#000000';
+     document.getElementById("y").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
+}
+function myfunc26()
+{
+     document.getElementById("z").style.background='#000000';
+     document.getElementById("z").disabled=true;
+     count--;
+     document.getElementById("lives").innerHTML=count;
+     if(count==0)
+{
+  window.alert('You Lost!!!')
+}
 }
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z'];  
 
-function myfunc()
-{
-     var alpha=document.
-}
+
